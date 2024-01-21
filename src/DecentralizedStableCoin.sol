7de3fdfdf177ad725c804495abfb6f47f04e91d6
0xhexbyte <mrigendrasoni26@gmail.com>
2024-01-21 07:06:07 -0800

    adding stuff

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.18;
+
+/*
+* @title Decentralized Stable Coin
+* @author hexbyte
+* Collateral: Exogenous
+* Minting: Algorithimic
+* Relative Stability: Pegged to USD
+*
+* This is the contract meant to be governed by DSCEngine. It is just ERC20 implementation of our stablecoin system.
+*
+*/
+contract DecentralizedStableCoin {}
