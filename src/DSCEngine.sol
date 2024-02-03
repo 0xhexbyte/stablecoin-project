cd14e821daeac4d2b144403563f329bfb2f0ab49
0xhexbyte <mrigendrasoni26@gmail.com>
2024-02-03 12:50:44 +0530

    adding stuff

---
+++ src/DSCEngine.sol
@@ -0,0 +1,80 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+
+/**
+ * @title DSCEngine
+ * @author hexbyte
+ * @notice The system is designed to be as minimal as possible and have the tokens maintain a 1 token == 1$ peg.
+ * The stablecoin has the properties:
+ * - Exogenous Collateral
+ * - Dollar pegged
+ * - Algorithmically Stable
+ * 
+ * It is similar to DAI if DAI had no governance, no fees and only backed by WETH and WBTC.
+ * 
+ * Our DSC system should always be overcollateralized. AT no point, should the collateral summed together be less 
+ * than or equal to the USD backed value of all the DSC.
+ * 
+ * @notice This contract is the core of the DSC System. It handles all the logic for mining and redeeming DSC, 
+ * as well as depositing and withdrawing collateral.
+ * @notice This contract is very loosely based on DAI on the MakerDAO DSS (DAI) system.
+ */
+contract DSCEngine {
+
+    /////////////////
+    ////ERRORS     //
+    /////////////////
+
+    error DSCEngine__NeedsMoreThanZero();
+
+
+
+    /////////////////
+    ////MODIFIERS  //
+    /////////////////
+
+    modifier moreThanZero(uint256 amount){
+        if (amount == 0){
+            revert DSCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
+
+
+
+    function depositCollateralAndMintDsc() external {
+
+    }
+
+    /**
+     * 
+     * @param tokenCollateralAddress The address of the token to deposit as collateral.
+     * @param amountCollateral The amount of collateral to deposit.
+     */
+
+    function depositCollateral(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+        ) external moreThanZero(amountCollateral) {
+
+        }
+
+    function redeemCollateralForDsc() external {}
+
+    function redeemCollateral() external {}
+    
+    function mintDsc() external {}
+
+    function burnDsc() external {}
+
+    function liquidate() external {}
+
+    function getHealthFactor() external view {}
+
+
+
+    
+
+}
